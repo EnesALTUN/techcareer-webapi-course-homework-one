7f45c4ef6a8a3a57ef5b4f2f52d82bb27b5d7e45
Enes ALTUN <ensalt_1998@hotmail.com>
2021-11-25 09:34:56 +0300

    added generic repository

---
+++ Library/Repository/GenericRepository.cs
@@ -0,0 +1,91 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Repository.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly HomeworkDbContext _context;
+        private readonly DbSet<TEntity> table;
+
+        public GenericRepository(HomeworkDbContext context)
+        {
+            _context = context;
+            table = _context.Set<TEntity>();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await table.ToListAsync();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await table.FindAsync(id);
+        }
+
+        public TEntity GetWithCriteria(Expression<Func<TEntity, bool>> filterExpression)
+        {
+            return table.Where(filterExpression).FirstOrDefault();
+        }
+
+        public async Task<TEntity> GetWithCriteriaAsync(Expression<Func<TEntity, bool>> filterExpression)
+        {
+            return await table.Where(filterExpression).FirstOrDefaultAsync();
+        }
+
+        public void Insert(TEntity obj)
+        {
+            table.Add(obj);
+        }
+
+        public async Task InsertAsync(TEntity obj)
+        {
+            await table.AddAsync(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            TEntity exist = table.Find(id);
+            table.Remove(exist);
+        }
+
+        public async Task DeleteAsync(object id)
+        {
+            TEntity exist = await table.FindAsync(id);
+            table.Remove(exist);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Library/Repository/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Repository.Infrastructure
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        List<TEntity> GetAll();
+        Task<List<TEntity>> GetAllAsync();
+        TEntity GetById(object id);
+        Task<TEntity> GetByIdAsync(object id);
+        TEntity GetWithCriteria(Expression<Func<TEntity, bool>> filterExpression);
+        Task<TEntity> GetWithCriteriaAsync(Expression<Func<TEntity, bool>> filterExpression);
+        void Insert(TEntity obj);
+        Task InsertAsync(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(object id);
+        Task DeleteAsync(object id);
+        void Save();
+        Task SaveAsync();
+    }
+}
